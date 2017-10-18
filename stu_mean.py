7498f2f2390d64dd5d3b9b7abc4d646c24d6be6f
Jake Zaia <jzaia@stuy.edu>
2017-10-17 22:06:23 -0400

    Added table for ids & avgs

--- stu_mean.py
+++ stu_mean.py
@@ -12,7 +12,8 @@ db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+# Populating the db
+#==========================================================
 
 # Populates courses
 c.execute("CREATE TABLE courses (code str, mark int, id int);")
@@ -49,6 +50,8 @@ def get_mean(stud):
         sid = each[0]
     Listy = get_grades(sid)
     thesum = sum(Listy)
+    if len(Listy) == 0: #Catch div by 0 error
+        return 0
     return thesum / len(Listy)
 
 def get_stud_info():
@@ -63,14 +66,37 @@ def get_stud_info():
             sid = each[0]
             info_list[count] += ", " + str(sid) + ", " + str(get_mean(name))
             count += 1
-    return info_list            
+    return info_list
 
-
+def add_table():
+    info = get_stud_info()
+    c.execute("CREATE TABLE peeps_avg (id int, avg int);")
+    for each in info:
+        each = each.split(',')
+        c.execute("INSERT INTO peeps_avg VALUES (%d, %d);"%(int(each[1]), int(each[2])))
+    
+    
+#==========================================================
+# Testing Functions
 #==========================================================
 
-print get_stud_info()
+print "Testing functions... \n"
+
+print "Getting grades for student 1:"
+print get_grades(1)
+
+print "\nGetting mean for student kruder:"
+print get_mean('kruder')
+
+print "\nPrinting info for all students..."
+for each in get_stud_info():
+    print each
+print '\n'
+
+print "Creating a table of ids & avgs..."
+add_table()
+for each in c.execute("SELECT * FROM peeps_avg;"):
+    print "%d: %d"%(each[0], each[1])
 
 db.commit() #save changes
 db.close()  #close database
-
-
